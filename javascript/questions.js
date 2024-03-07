1c8eee27d1f4d58515da0f768541747602d5e441
Simen Wang <simen@DESKTOP-K3GB1OD.intra.entrade.no>
2024-03-07 15:30:56 +0100

    added java for questions, updated css

---
+++ javascript/questions.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+        Numb: 1,
+        question: "Hvem har forfølgingsrett?",
+        answer: "A. Alle jegere som på lovlig grunn sårer storvilt",
+        options: [
+        "A. Alle jegere som på lovlig grunn sårer storvilt",
+        "B. Alle jegere som på lovlig grunn sårer vilt",
+        "C. Bare jaktoppsynet"
+        ]
+    },
+    {
+        Numb: 2,
+        question: "Under jakt på elg kommer du over en ku med kalv. Hva gjør du?",
+        answer: "A. Skyter kalven før elgkua",
+        options: [
+            "A. Skyter kalven før elgkua",
+            "B. Skyter elgkua før kalven",
+            "C. Det er ulovlig å skyte på en mor med avkom",
+        ]
+    },
+    {
+        Numb: 3,
+        question: "Hva regnes som vitalt organ på et vilt?",
+        answer: "B. Lunger",
+        options: [
+            "A. Nakke",
+            "B. Lunger",
+            "C. Spiserør"
+        ]
+    },
+    {
+        Numb: 4,
+        question: "Hvordan skal avlivning skje?",
+        answer: "B. Slik at viltet ikke kommer i fare for å lide unødvendig",
+        options: [
+            "A. Slik at viltet dør momentant",
+            "B. Slik at viltet ikke kommer i fare for å lide unødvendig",
+            "C. Slik at kjøttet blir minst mulig ødelagt"
+        ]
+    },
+    {
+        Numb: 5,
+        question: "Kravet om tilgang til godkjent ettersøkshund gjelder ved:",
+        answer: "C. Jakt på elg, hjort og rådyr",
+        options: [
+            "A. Jakt på storvilt",
+            "B. All jakt",
+            "C. Jakt på elg, hjort og rådyr"
+      ]
+    }
+];
